53fde6c711ae2c279ec920be6497042b39ce27a6
mimilabomb <bartoszsz13@gmail.com>
2024-06-05 21:00:50 +0200

    end 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,52 @@
 
 /* rozwiązanie z pętlą for */
 const x = 4;
+const z = Number(prompt('Podaj liczę!'))
 
 
+
+if(z>=1&&z<9){
+    for(let i = 1; i <= 9; i++){
+        let result = i * z;
+        console.log(z + ' x '+ i + ' = ' + result);
+    }
+}else{
+    console.log('Podałeś błędną liczbę');
+}
+ 
 
 /* rozwiązanie z pętlą while  */
+const a = Number(prompt('Podaj podstawę potęgowania!'));;
+const n = Number(prompt('Podaj wykładnik!'));
+
+let iter = 0;
+let result = 1;
+let info ='';
+while(iter < n){
+    
+    result *= a
+
+   if(iter > 0){
+    info += ' * '
+   }
+   info += a ;
+   iter++;
+}
+info = info + ' = ' + result;
+
+
+console.log(info);
+
+// let j = 1;
+// let toNumber = 9;
+
+// while(j <== toNumber){
+
+//     let resultWhile = j * x;
+
+//     console.log(x + ' x '+ toNumber + ' = ' + resultWhile);
+// }
+
+
+
+
